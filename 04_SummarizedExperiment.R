162554dd9e4830fb2fc8a56e7beb9b3047ad3063
Dara Jahzeel Palafox collado <darapc@lcg.unam.mx>
2026-02-12 12:11:33 -0600

    Se modifoco el contenido del archivo para poder realizar las practicas de la clase.

--- 04_SummarizedExperiment.R
+++ 04_SummarizedExperiment.R
@@ -14,22 +14,22 @@ set.seed(20210223)
 counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
 ## Información de nuestros genes
 rowRanges <- GRanges(
-    rep(c("chr1", "chr2"), c(50, 150)),
-    IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
-    strand = sample(c("+", "-"), 200, TRUE),
-    feature_id = sprintf("ID%03d", 1:200)
+  rep(c("chr1", "chr2"), c(50, 150)),
+  IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
+  strand = sample(c("+", "-"), 200, TRUE),
+  feature_id = sprintf("ID%03d", 1:200)
 )
 names(rowRanges) <- paste0("gene_", seq_len(length(rowRanges)))
 ## Información de nuestras muestras
 colData <- DataFrame(
-    Treatment = rep(c("ChIP", "Input"), 3),
-    row.names = LETTERS[1:6]
+  Treatment = rep(c("ChIP", "Input"), 3),
+  row.names = LETTERS[1:6]
 )
 ## Juntamos ahora toda la información en un solo objeto de R
 rse <- SummarizedExperiment(
-    assays = SimpleList(counts = counts),
-    rowRanges = rowRanges,
-    colData = colData
+  assays = SimpleList(counts = counts),
+  rowRanges = rowRanges,
+  colData = colData
 )
 
 ## Exploremos el objeto resultante
@@ -69,7 +69,6 @@ rse[, c("A", "D", "F")]
 # library("iSEE")
 # iSEE::iSEE(rse)
 
-
 ## ----download_sce_layer---------------------------------------------------------
 ## Descarguemos unos datos de spatialLIBD
 sce_layer <- spatialLIBD::fetch_data("sce_layer")
@@ -78,7 +77,28 @@ sce_layer
 ## Revisemos el tamaño de este objeto
 lobstr::obj_size(sce_layer)
 
-
 ## ----explore_sce_layer, eval = FALSE--------------------------------------------
 # iSEE::iSEE(sce_layer)
 
+## ---Ejercico boxplot ------------------------------------------------------------
+x <- assay(rse)["gene_3", ]
+y <- rse$Treatment
+boxplot(x ~ y)
+summary(lm(x ~ y))
+
+## FIRST ISEE ---------------------------------------------------------------------
+## Explora el objeto rse de forma interactiva
+library("iSEE")
+iSEE::iSEE(rse)
+
+## Descarguemos unos datos de spatialLIBD
+sce_layer <- spatialLIBD::fetch_data("sce_layer")
+sce_layer
+
+## Revisemos el tamaño de este objeto
+lobstr::obj_size(sce_layer)
+
+## Al igual que nuestro objeto rse podemos usar iSEE::iSEE() para explorar los datos.
+iSEE::iSEE(sce_layer)
+
+## -- Ejercicio plots Meli - Pendiente.
